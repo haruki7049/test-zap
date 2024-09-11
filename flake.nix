b379a475bbb725ca7d1c096703ed3dcbfe2ce099
haruki7049 <tontonkirikiri@gmail.com>
2024-09-11 14:09:08 +0900

    chore(flake.nix): Edit

--- flake.nix
+++ flake.nix
@@ -11,24 +11,8 @@
         pkgs = import nixpkgs {
           inherit system;
         };
-        stdenv = pkgs.stdenv;
         treefmtEval = treefmt-nix.lib.evalModule pkgs ./treefmt.nix;
         zig = pkgs.zig_0_13;
-        test-zap = stdenv.mkDerivation {
-          pname = "test-zap";
-          version = "dev";
-
-          src = ./.;
-
-          nativeBuildInputs = [
-            zig.hook
-          ];
-
-          zigBuildFlags = [
-            "-Doptimize=Debug"
-          ];
-        };
-        example-numbers = pkgs.callPackage ./examples/numbers { };
       in
       {
         # Use `nix fmt`
@@ -36,14 +20,7 @@
 
         # Use `nix flake check`
         checks = {
-          inherit test-zap;
           formatting = treefmtEval.config.build.check self;
-        };
-
-        # nix build .
-        packages = {
-          inherit test-zap;
-          default = test-zap;
         };
 
         devShells.default = pkgs.mkShell {
